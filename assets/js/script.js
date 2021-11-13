b5763ee43f2a9523e241689f380155f6534d4498
calvin <cal.villa@icloud.com>
2021-11-13 14:43:52 -0500

    able to generate password

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,62 @@
-// Assignment code here
-var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-var specialCharacters = ["@", "%", "+", "/", "'", "!", "#", "$", "^", "?", ":", ",", "(", ")", "{", "}", "[", "]", "~", "-", "_", "."]
-
-function generatePassword() {
-
-  var passLength = prompt("Please choose the length of your password? 8 to 128 characters.");
-    if (passLength < 8 || passLength <=128){
-      alert("Please select a number between 8 and 128")
-      return generatePassword
-    }
-
-  var passUpperCase = confirm("Would you like to include Upper Case Letters?");
-  console.log(passUpperCase)
- 
-  var passLowerCase = confirm("Would you like to include Lower Case Letters?");
-  console.log(passLowerCase)
-
-  var passNum = confirm("Would you like to include Numbers?");
-  console.log(passNum)
-
-  var passSpecChar = confirm("Would you like to include Special Characters?");
-  console.log(passSpecChar)
-}
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// Assignment code here
+function generatePassword() {
 
+  var passLength = prompt("Please choose the length of your password? 8 to 128 characters.");
+  if (passLength < 8 || passLength > 128) {
+    alert("Please select a number between 8 and 128");
+    return generatePassword();
+  }
+  if (passLength >= 8 && passLength <= 128) {
+    var passUpperCase = confirm("Would you like to include Upper Case Letters?");
+    var passLowerCase = confirm("Would you like to include Lower Case Letters?");
+    var passNum = confirm("Would you like to include Numbers?");
+    var passSpecChar = confirm("Would you like to include Special Characters?");
+  }
 
+  if (passUpperCase == false && passLowerCase == false && passNum == false && passSpecChar == false) {
+    alert("Please reselect your password criteria.");
+    return generatePassword();
+  }
+
+  if (passUpperCase) {
+    var passUpperCase = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"]
+  }
+  if (passUpperCase = false) {
+    var passUpperCase = [""];
+  }
+  if (passLowerCase) {
+    var passLowerCase = ["abcdefghijklmnopqrstuvwxyz"]
+  }
+  if (passLowerCase = false) {
+    var passLowerCase = [""];
+  }
+  if (passNum) {
+    var passNum = ["0123456789"]
+  }
+  if (passNum = false) {
+    var passNum = [""]
+  }
+  if (passSpecChar) {
+    var passSpecChar = ["!@#$%^&*()"]
+  }
+  if (passNum = false) {
+    var passNum = [""]
+  }
+
+  var randomCharacters = passUpperCase + passLowerCase + passNum + passSpecChar
+
+  var password = "";
+  for (var i = 0; i < passLength; i++) {
+    var trueRandom = randomCharacters[Math.floor(Math.random() * randomCharacters.length)];
+    password += trueRandom
+  }
+
+  //returns the generated password
+  return password
+}
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -38,3 +64,5 @@ function writePassword() {
 
   passwordText.value = password;
 }
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
